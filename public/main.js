469cccdc708d60c661f832335ac1d147e9dd2493
Brajesh <brajesh.mymailbox@gmail.com>
2017-03-21 23:01:46 +0800

    Ui Tweaks

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to ChatBot";
     log(message, {
       prepend: true
     });
